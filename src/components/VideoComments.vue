8416f638cce1f0c62a87a2194b68b5b9e586dd45
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-20 09:50:56 +0100

    style: added padding to comments component

--- src/components/VideoComments.vue
+++ src/components/VideoComments.vue
@@ -3,12 +3,14 @@
 </script>
 
 <template>
-  <div>
+  <div class="comments">
     <h1>Video Comments</h1>
   </div>
 
 </template>
 
 <style scoped>
-
+    .comments {
+        padding: 1rem;
+    }
 </style>
